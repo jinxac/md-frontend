4fc713b2b09bf266ddc111f1677fcd5b95c46b79
Harkirat Saluja <saluja.harkirat@gmail.com>
2019-10-29 16:13:30 +0530

    base setup + map package

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  GoogleApiWrapper,
+  Map
+} from "google-maps-react";
 
-function App() {
+const propTypes = {
+  google: PropTypes.object.isRequired
+};
+
+
+
+function App ({google}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map google={google} zoom={14} />
   );
 }
 
-export default App;
+App.propTypes = propTypes;
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCGSqfC1AC1UgsvgZl2uH3QyQBZ8uFOu38"
+})(App);
